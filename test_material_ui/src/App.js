0b77b3c790c3d62e21a41b31ac38926e72f8bd79
TanKARASU <karasutan@gmail.com>
2021-03-16 05:31:39 +0100

    initial commmit

---
+++ test_material_ui/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+
+function App() {
+  return (
+    <div className="App">
+      <Box className="test">
+        <Button variant="contained" color="primary">
+          Click this
+        </Button>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
